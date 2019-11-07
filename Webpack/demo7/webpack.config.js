6877073ccaf66870ea76d86fc94153fdefd4a819
yerong <241761025@qq.com>
2019-11-07 18:36:30 +0800

    no message

--- Webpack/demo7/webpack.config.js
+++ Webpack/demo7/webpack.config.js
@@ -10,7 +10,7 @@ module.exports={
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            template:__dirname+"/src/index.html"
         }),
         new webpack.NamedModulesPlugin()
     ],
